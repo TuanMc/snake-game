2c19a38d07cdbcc6cb27a579df8a9582869ef5f0
TuanMc <maichungtuan@gmail.com>
2020-09-13 16:56:36 +0700

    move()

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const snakeboard_ctx = snakeboard.getContext("2d");
 const snakeboard_w = snakeboard.width;
 const snakeboard_h = snakeboard.height;
 const score = 0;
+const snake_part_w = 10;
+const dx = 10;
+const dy = 0;
+const speed = 200; // miliseconds
 
 // Snake variables
 const snake_parts = [
@@ -17,26 +21,41 @@ const snake_parts = [
   { x: 170, y: 200 },
   { x: 160, y: 200 },
 ];
-const snake_part_w = 10;
 
 function drawBackground() {
   // Draw background
-  snakeboard_ctx.fillRect(0, 0, snakeboard_w, snakeboard_h);
   snakeboard_ctx.fillStyle = bg_color;
+  snakeboard_ctx.fillRect(0, 0, snakeboard_w, snakeboard_h);
 
   // Draw score text
   snakeboard_ctx.font = "20px Comic Sans MS";
   snakeboard_ctx.fillStyle = "white";
   snakeboard_ctx.fillText("Score: " + score, 10, 30);
-
 }
 
 function drawSnake() {
   snake_parts.forEach((p) => {
-    snakeboard_ctx.fillRect(p.x, p.y, snake_part_w, snake_part_w);
     snakeboard_ctx.fillStyle = snake_part_color;
+    snakeboard_ctx.fillRect(p.x, p.y, snake_part_w, snake_part_w);
   });
 }
 
-drawBackground();
-drawSnake();
+function move() {
+  const head = {
+    x: snake_parts[0].x + dx,
+    y: snake_parts[0].y + dy,
+  };
+
+  snake_parts.unshift(head);
+  snake_parts.pop();
+}
+
+function start() {
+  setInterval(() => {
+    drawBackground();
+    move();
+    drawSnake();
+  }, speed);
+}
+
+start();
