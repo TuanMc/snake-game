8d87ecf01b37ae2899d0efad6c3c15792f59e339
TuanMc <maichungtuan@gmail.com>
2020-09-19 11:48:57 +0700

    change direction

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const snakeboard_w = snakeboard.width;
 const snakeboard_h = snakeboard.height;
 const score = 0;
 const snake_part_w = 10;
-const dx = 10;
-const dy = 0;
 const speed = 200; // miliseconds
+var dx = 10;
+var dy = 0;
 
 // Snake variables
 const snake_parts = [
@@ -50,12 +50,65 @@ function move() {
   snake_parts.pop();
 }
 
+function hasGameEnd() {
+  for (let i = 4; i < snake_parts.length; i++) {
+    if (
+      snake_parts[i].x === snake_parts[0].x &&
+      snake_parts[i].y === snake_parts[0].y
+    )
+      return true;
+  }
+  const hitLeftWall = snake_parts[0].x < 0;
+  const hitRightWall = snake_parts[0].x > snakeboard.width - 10;
+  const hitToptWall = snake_parts[0].y < 0;
+  const hitBottomWall = snake_parts[0].y > snakeboard.height - 10;
+  return hitLeftWall || hitRightWall || hitToptWall || hitBottomWall;
+}
+
 function start() {
-  setInterval(() => {
+  if (hasGameEnd()) {
+    alert("Game end!");
+    return;
+  }
+  setTimeout(() => {
     drawBackground();
     move();
     drawSnake();
+    start();
   }, speed);
 }
 
+function changeDirection(event) {
+  const LEFT_KEY = 37;
+  const RIGHT_KEY = 39;
+  const UP_KEY = 38;
+  const DOWN_KEY = 40;
+
+  // Check current direction => Not change direction if snake go reverse
+  const goingUp = dy === -10;
+  const goingDown = dy === 10;
+  const goingRight = dx === 10;
+  const goingLeft = dx === -10;
+
+  const keyPressed = event.keyCode;
+
+  if (keyPressed === LEFT_KEY && !goingRight) {
+    dx = -10;
+    dy = 0;
+  }
+  if (keyPressed === UP_KEY && !goingDown) {
+    dx = 0;
+    dy = -10;
+  }
+  if (keyPressed === RIGHT_KEY && !goingLeft) {
+    dx = 10;
+    dy = 0;
+  }
+  if (keyPressed === DOWN_KEY && !goingUp) {
+    dx = 0;
+    dy = 10;
+  }
+}
+
 start();
+document.addEventListener("keydown", changeDirection);
